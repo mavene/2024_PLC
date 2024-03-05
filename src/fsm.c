04b54275e9a6fcbb8df6b573a696f4aba66ee05c
mavene <dianmaisara@gmail.com>
2024-03-05 10:13:40 +0800

    Done - base skeleton of states and FSM struct

--- src/fsm.c
+++ src/fsm.c
@@ -1,2 +1,34 @@
 /* Macro FSM - main flow of program here 
    (Might combine with main.. but we'll see...) */
+
+typedef enum {
+    START,
+    IDLE,
+    UPLOAD_MODE,
+    PARSER_PPM,
+    EDIT_MODE,
+    EDIT_TRANSFORMATION,
+    EDIT_ROTATION,
+    EDIT_SCALING,
+    EDIT_EDGE_DETECT,
+    PREVIEW_MODE,
+    DOWNLOAD_MODE,
+    ERROR,
+    END  
+} State;
+
+/*
+    Define the FSM object as a struct.
+    State: Current state of the FSM.
+*/
+typedef struct {
+    State currentState;
+} FSM;
+
+/*
+    Initialize the FSM before running an input string.
+    Simply set the state attribute to ???.
+*/
+void initFSM(FSM *fsm) {
+    fsm->currentState = START;
+}

787d3cabad780c432aca308845c606645c65210c
NithyaRV <127846590+NithyaRV@users.noreply.github.com>
2024-03-14 15:24:33 +0800

    Updated file to be compatible with parser.h

--- include/transformations.h
+++ include/transformations.h
@@ -1,6 +1,7 @@
+#include "parser.h"
 #define PI 3.14159265
 #define FILL 0
 #define RADTOANG 57.29577951
-typedef struct {
-    unsigned char r, g, b;
-} Pixel;
+Pixel bilinear_interpolate(double x, double y, long cols, long rows, Pixel **the_image);
+void EDIT_Rotate(Pixel **image, Pixel **rotatedImage, int rows, int cols, float angle);
+void EDIT_Scale(Pixel **image, Pixel **scaledImage, int preRows, int preCols, int scale);

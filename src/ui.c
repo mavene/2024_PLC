ff827d5e6b04b30cb96b5bfbef643c7a396f5ec7
Ravenix12 <shivani_mariappan@mymail.sutd.edu.sg>
2024-03-20 01:46:06 +0800

    basic gtk
    ^^

--- src/ui.c
+++ src/ui.c
@@ -1,7 +1,63 @@
+#include <stdio.h>
+#include <gtk/gtk.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
+
+
 /* UI here, including preview's redo and undo feature */
 
 /* SHIVANI */
 /* Put in scanf for file paths and sanitize them here*/
 char * retrieve_file() {
     // Call GTK here to retrieve file 
+}
+
+static void
+print_hello (GtkWidget *widget, gpointer data)
+{
+  g_print ("Previous\n");
+}
+
+static void activate (GtkApplication *app, gpointer user_data)
+{
+  GtkWidget *window;
+  GtkWidget *button;
+  GtkWidget *image;
+  GtkWidget *box;
+
+  window = gtk_application_window_new (GTK_APPLICATION (app));
+  gtk_window_set_title (GTK_WINDOW (window), "Preview");
+  gtk_window_set_default_size (GTK_WINDOW (window), 500, 500);
+
+  box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
+  gtk_container_add (GTK_CONTAINER (window), box);
+  gtk_widget_set_valign (box, GTK_ALIGN_CENTER);
+  gtk_widget_set_halign (box, GTK_ALIGN_CENTER);
+
+  image = gtk_image_new_from_file("image.png"); 
+  gtk_box_pack_start (GTK_BOX (box), image, FALSE, FALSE, 0);
+  gtk_widget_set_halign (image, GTK_ALIGN_CENTER);
+  gtk_widget_set_valign (image, GTK_ALIGN_CENTER);
+  
+  button = gtk_button_new_with_label ("Hello World");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  gtk_box_pack_start (GTK_BOX (box), button, FALSE, FALSE, 0);
+  gtk_widget_set_halign (button, GTK_ALIGN_CENTER);
+  gtk_widget_set_valign (button, GTK_ALIGN_CENTER);
+  gtk_widget_set_size_request (button, 100, 50);
+
+  gtk_widget_show_all (window);
+}
+
+
+int main (int argc, char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
 }

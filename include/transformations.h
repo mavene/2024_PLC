de92f9189bee5b77e24cb229afeacc69543616c3
mavene <dianmaisara@gmail.com>
2024-03-28 23:23:11 +0800

    Deconflict transformations.h

--- include/transformations.h
+++ include/transformations.h
@@ -1,8 +1,7 @@
-#define _USE_MATH_DEFINES
-#include <math.h>
 #include "parser.h"
-
+#define PI 3.14159265
 #define FILL 0
 #define RADTOANG 57.29577951
-
-void rotateImage(Pixel**, Pixel**, int, int, float);
+Pixel bilinear_interpolate(double x, double y, long cols, long rows, Pixel **the_image);
+void EDIT_Rotate(Pixel **image, Pixel **rotatedImage, int rows, int cols, float angle);
+void EDIT_Scale(Pixel **image, Pixel **scaledImage, int preRows, int preCols, int scale);

18a9f30b6c8297a15919f51ebde356651f81a90f
NithyaRV <127846590+NithyaRV@users.noreply.github.com>
2024-04-06 18:09:34 +0800

    Updated scale function

--- include/transformations.h
+++ include/transformations.h
@@ -9,6 +9,6 @@ void EDIT_Rotate_1(const Image*, Image*, float);
 void EDIT_Rotate(const Image*, Image*, float);
 void EDIT_Rotate_new(const Image*, Image*, float);
 void EDIT_Transformation(const Image*, Image*, int, int, int cropWidth, int cropHeight, int dx, int dy);
-void EDIT_Scale(const Image *image, Image *scaledImage, int scale); 
+void EDIT_Scale(const Image *image, Image *scaledImage, int scale, int corner);
 void EDIT_Edgedetection(const Image *image, Image *edgeImage);
 void EDIT_CalcRotatedDimensions(const Image *image, int*, int*, float );

14a0a4bcb86d98361d81fcbfec1798fbc2aa8a14
mavene <dianmaisara@gmail.com>
2024-04-08 14:39:40 +0800

    Remove build artifacts

--- obj/history.d
+++
@@ -1,5 +0,0 @@
-obj/history.o: src/history.c include/history.h include/parser.h \
- include/state.h
-include/history.h:
-include/parser.h:
-include/state.h:
--- obj/parser.d
+++
@@ -1,7 +0,0 @@
-obj/parser.o: src/parser.c include/parser.h include/state.h include/fsm.h \
- include/history.h include/parser.h
-include/parser.h:
-include/state.h:
-include/fsm.h:
-include/history.h:
-include/parser.h:
